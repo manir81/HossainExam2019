4fe3485626a35394016be35a37bfd15462dd81b6
manir81 <47307182+manir81@users.noreply.github.com>
2019-03-06 12:22:33 -0500

    Add files via upload

---
+++ Manager.java
@@ -0,0 +1,16 @@
+package design;
+
+public abstract class Manager {
+    final String dept2 ="HR";
+
+    final String dept3 = "Qa";
+    final String dept4="Finance";
+    public abstract void Manager();
+
+    public abstract void AllDepart();
+    public static void ManagerSalary() {
+        System.out.println("this is manager salary:");
+    }
+}
+
+
