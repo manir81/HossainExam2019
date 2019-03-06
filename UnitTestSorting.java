76e753f8de9340088eacd80e84e5d90b50192cad
manir81 <47307182+manir81@users.noreply.github.com>
2019-03-06 11:40:29 -0500

    Add files via upload

---
+++ UnitTestSorting.java
@@ -0,0 +1,87 @@
+package algorithm;
+
+
+import org.testng.Assert;
+
+public class UnitTestSorting {
+
+    /*
+      This class is about Unit testing for Sorting Algorithm.
+     */
+    public static void main(String[] args) {
+        int[] unSortedArray = {6, 9, 2, 5, 1, 0, 4};
+        int[] sortedArray = {0, 1, 2, 4, 5, 6, 9};
+
+        Sort sort = new Sort();
+
+        sort.selectionSort(unSortedArray);
+
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
+            System.out.println("Array was Sorted");
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+
+        sort.insertionSort(unSortedArray);
+
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
+            System.out.println("Array was Sorted");
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+        sort.bubbleSort(unSortedArray);
+
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
+            System.out.println("Array was Sorted");
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+
+        sort.heapSort(unSortedArray);
+
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
+            System.out.println("Array was Sorted");
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+        sort.bucketSort(unSortedArray);
+
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
+            System.out.println("Array was Sorted");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        //sort.quickSort(unSortedArray);
+
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
+            System.out.println("Array was Sorted");
+        }catch(Exception ex){
+            ex.getMessage();
+        }
+
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
+            System.out.println("Array was Sorted");
+        }catch(Exception ex) {
+            ex.getMessage();
+        }
+
+        sort.shellSort(unSortedArray);
+
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
+            System.out.println("Array was Sorted");
+        }catch(Exception ex){
+            ex.getMessage();
+        }
+
+
+    }
+}
